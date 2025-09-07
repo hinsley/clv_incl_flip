58a80d2ea8f051311720c8de1d9c1d2df07ddda8
Carter Hinsley <carterhinsley@outlook.com>
2025-09-07 15:50:17 -0400

    Add 6D Plant model

--- plant_clv.jl
+++ plant_clv.jl
@@ -16,9 +16,10 @@ using OrdinaryDiffEq
 # 1) Configurable parameters
 # You can modify `gh` to change the hyperpolarization-activated current conductance (p[4]).
 # You can also modify Δx (p[16]) and ΔCa (p[17]).
-gh = Float64(Plant.default_params[4])
-ΔCa = 15.0
-Δx  = -2.4
+gh = 0.005 # Float64(Plant.default_params[4])
+τh = Float64(Plant.default_params[14])
+ΔCa = -40.0
+Δx  = -1.0
 
 # 2) System
 # Build initial condition by inserting y between x and n. We use y0 = hinf(V0).
@@ -36,6 +37,7 @@ end
 # Update parameters with configured gh (index 4), Δx (index 16), and ΔCa (index 17).
 p = collect(Float64.(Plant.default_params))
 p[4] = gh
+p[14] = τh
 p[16] = Δx
 p[17] = ΔCa
 
@@ -108,14 +110,40 @@ rowsize!(fig.layout, 2, Relative(0.2))
 
 lines!(ax, trajectory_dense, linewidth=0.3)
 
-stride     = 1000
 scale      = 0.06
-V_scale    = 160
+V_scale    = 100
+n_arrows   = 100000   # Number of arclength-sampled points to draw CLVs at.
 clv_colors = (:red, :orange, :green, :blue, :purple, :brown)
 min_proj_norm = 1e-8
 
+# Compute arclength in displayed (Ca, x, V/V_scale) space and pick n_arrows points.
+Nkeep = length(Gamma)
+speeds = Vector{Float64}(undef, Nkeep)
+for i in 1:Nkeep
+    du = Plant.melibeNew(xs[i], p, ts[i])
+    # (Ca, x, V/V_scale) speed for visual arclength sampling.
+    speeds[i] = norm(SVector(du[5], du[1], du[6]/V_scale))
+end
+
+arc = zeros(Float64, Nkeep)
+for i in 2:Nkeep
+    dt_i = ts[i] - ts[i-1]
+    arc[i] = arc[i-1] + 0.5 * (speeds[i-1] + speeds[i]) * dt_i
+end
+
+total_arc = arc[end]
+targets = range(0, stop=total_arc, length=n_arrows)
+sel_indices = Int[]
+for a in targets
+    idx = searchsortedfirst(arc, a)
+    if 1 <= idx <= Nkeep
+        push!(sel_indices, idx)
+    end
+end
+unique!(sel_indices)
+
 # Origins in 3D: (Ca, x, V)
-origins = [Point3f(u[5], u[1], u[6]) for u in xs[1:stride:end]]
+origins_sel = [Point3f(u[5], u[1], u[6]) for u in (xs[i] for i in sel_indices)]
 
 # 7) Compute subspace angle θ(t): span{Γ₁,Γ₂} vs span{Γ₃,…,Γ₆} in full state space
 times_rel = [t - ts[1] for t in ts]
@@ -134,18 +162,19 @@ for i in 1:length(Gamma)
     angles[i] = acos(sigma_max)
 end
 
-real_j = [1, 0, 2, 3, 4, 5] # Hack: Since we don't use y it is skipped.
+# real_j = [1, 0, 2, 3, 4, 5] # Hack: Since we don't use y it is skipped when gh=0.
+real_j = 1:nclv
 for j in 1:nclv
     origins_j = Point3f[]
     dirs_j = Vec3f[]
-    for (k, i) in enumerate(1:stride:length(Gamma))
+    for (k, i) in enumerate(sel_indices)
         vfull = Gamma[i][:, j]
         v3d = @SVector Float64[vfull[5], vfull[1], vfull[6]/V_scale]
         nv = norm(v3d)
         if nv >= min_proj_norm
             v3n = (v3d ./ nv) .* scale
             push!(dirs_j, Vec3f(Float32(v3n[1]), Float32(v3n[2]), Float32(v3n[3]*V_scale)))
-            push!(origins_j, origins[k])
+            push!(origins_j, origins_sel[k])
         end
     end
     if !isempty(dirs_j)
